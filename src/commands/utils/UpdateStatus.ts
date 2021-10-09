c18a340a05f61756ad80e0a2a13bdc58924a0919
Kopymatic <creepcipher@gmail.com>
2021-10-08 18:57:07 -0600

    Unfinished UpdateStatus command

---
+++ src/commands/utils/UpdateStatus.ts
@@ -0,0 +1,31 @@
+import { Message, TextableChannel } from "eris";
+import { KopyCommand } from "../../utilities/KopyCommand";
+import {global} from "../../main/global"
+import { Types, Styles } from "../../utilities/Components";
+
+/*
+----------
+UNFINISHED
+----------
+*/
+
+export class UpdateStatus extends KopyCommand {
+    constructor() {
+        super()
+        this.label = "UpdateStatus";
+        this.options = {
+            description: "Updates the bots status", 
+            aliases: ["us"],
+            hidden: true,
+            caseInsensitive: true
+        };
+        this.generator = (msg, args) => this.run(msg, args)
+    }
+
+    override async run(msg: Message<TextableChannel>, args: string[]): Promise<string> {
+        if(msg.author.id == "326489320980611075") { //TODO make this use a list from a json or something
+            global.bot.shards.entries()
+        }
+        return null
+    }
+}
