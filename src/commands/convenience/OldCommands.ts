3c215c6c46d414a5de4607b936b6f532fb2e49fe
Kopy <creepcipher@gmail.com>
2022-03-09 10:58:31 -0700

    add OldCommands so that i can delete old commands

---
+++ src/commands/convenience/OldCommands.ts
@@ -0,0 +1,109 @@
+import { SlashCommand } from "@kopymatic/basebot";
+
+export class addspaces extends SlashCommand {
+    constructor() {
+        super();
+        this.name = "addspaces";
+        this.toDelete = true;
+        this.description = "old";
+        this.onRun = () => {
+            console.log("PEEEPEEE");
+        };
+    }
+}
+
+export class alphabetizer extends SlashCommand {
+    constructor() {
+        super();
+        this.name = "alphabetizer";
+        this.toDelete = true;
+        this.description = "old";
+        this.onRun = () => {
+            console.log("PEEEPEEE");
+        };
+    }
+}
+
+export class buttonexample extends SlashCommand {
+    constructor() {
+        super();
+        this.name = "button-example";
+        this.toDelete = true;
+        this.description = "old";
+        this.onRun = () => {
+            console.log("PEEEPEEE");
+        };
+    }
+}
+
+export class clap extends SlashCommand {
+    constructor() {
+        super();
+        this.name = "clap";
+        this.toDelete = true;
+        this.description = "old";
+        this.onRun = () => {
+            console.log("PEEEPEEE");
+        };
+    }
+}
+
+export class owo extends SlashCommand {
+    constructor() {
+        super();
+        this.name = "owo";
+        this.toDelete = true;
+        this.description = "old";
+        this.onRun = () => {
+            console.log("PEEEPEEE");
+        };
+    }
+}
+
+export class randomcaps extends SlashCommand {
+    constructor() {
+        super();
+        this.name = "randomcaps";
+        this.toDelete = true;
+        this.description = "old";
+        this.onRun = () => {
+            console.log("PEEEPEEE");
+        };
+    }
+}
+
+export class reverser extends SlashCommand {
+    constructor() {
+        super();
+        this.name = "reverser";
+        this.toDelete = true;
+        this.description = "old";
+        this.onRun = () => {
+            console.log("PEEEPEEE");
+        };
+    }
+}
+
+export class say extends SlashCommand {
+    constructor() {
+        super();
+        this.name = "say";
+        this.toDelete = true;
+        this.description = "old";
+        this.onRun = () => {
+            console.log("PEEEPEEE");
+        };
+    }
+}
+
+export class scramble extends SlashCommand {
+    constructor() {
+        super();
+        this.name = "scramble";
+        this.toDelete = true;
+        this.description = "old";
+        this.onRun = () => {
+            console.log("PEEEPEEE");
+        };
+    }
+}
